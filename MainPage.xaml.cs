065626d549cdb4650baabaf2b2b811805cfc4a5e
Filip Navara <navara@emclient.com>
2021-12-10 21:23:48 +0100

    Crashing MAUI app with pinned MAUI version

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace MauiApp1
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			/*count++;
+			CounterLabel.Text = $"Current count: {count}";
+
+			SemanticScreenReader.Announce(CounterLabel.Text);*/
+			Img1.Source = ImageSource.FromStream(() => new MemoryStream(Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg==")));
+		}
+	}
+}
